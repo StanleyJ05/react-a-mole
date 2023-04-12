1af2f7d7513b1ed621f01dea947ae7140cd88593
Stanley Jackson <stanleyjackson11.sj@gmail.com>
2023-04-11 21:37:02 -0700

    finished

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import MoleContainer from './components/MoleContainer';
+
+import './App.css';
+
+function App() {
+  let [score, setScore] = useState(0)
+
+  const createMoleHill = (numHills = 9) => {
+    let hills = [];
+    for(let i = 0; i < numHills; i++) {
+      hills.push(
+        <MoleContainer
+        key={i}
+        setScore={setScore}
+        score={score}
+        />
+      );
+    }
+    return(
+      <div>
+        {hills}
+      </div>
+    )
+  }
+  return (
+    <div className="App">
+      <p>{score}</p>
+      {createMoleHill()}
+    </div>
+  );
+}
+
+export default App;
